c553e019d6d705b006c6642b66eefcb8d08ca631
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-11-17 01:01:32 -0300

    Evoluindo no Vue-JS 1.0

--- main.js
+++ main.js
@@ -1,22 +1,60 @@
 const app = Vue.createApp({
     data() {
         return {
-            firstName: 'Marcos',
-            lastName: 'Guimarães',
-            email: 'marcos.grocha@campuscode.com.br',
-            city: 'Aracaju',
-            picture: 'https://randomuser.me/api/portraits/men/3.jpg',
+            listContacts: [
+                {
+                    firstName: 'Roberto',
+                    lastName: 'Soares',
+                    email: 'roberto@campuscode.com.br',
+                    city: 'São Roque',
+                    picture: 'https://randomuser.me/api/portraits/men/58.jpg'
+                },
+                {
+                    firstName: 'Renata',
+                    lastName: 'Soares',
+                    email: 'renata@campuscode.com.br',
+                    city: 'São Paulo',
+                    picture: 'https://randomuser.me/api/portraits/women/24.jpg'
+                },
+                {
+                    firstName: 'Marcos',
+                    lastName: 'Guimarães',
+                    email: 'marcos.grocha@campuscode.com.br',
+                    city: 'Aracaju',
+                    picture: 'https://randomuser.me/api/portraits/men/3.jpg'
+                }
+            ]
         }
     },
     methods: {
-        changeData() {
-            this.firstName = 'Caie'
-            this.lastName = 'Duarte'
-            this.email = 'caio.duarte@campuscode.com.br'
-            this.city = 'Cajamar'
-            this.picture = 'https://randomuser.me/api/portraits/women/65.jpg'
+        async getData() {
+            let response = await fetch('https://randomuser.me/api/?results=15');
+            
+            // Exibindo os dados no console do navegador
+            // console.log(response.json());
+
+            // Armazenando os dados do json
+            let data = await response.json();
+            // console.log(data);
+
+            // Removendo todos os itens do array listContacts
+            this.listContacts = [];
+
+            data.results.forEach(item => {
+                var contact = new Object();
+                
+                contact.picture = item.picture.large;
+                contact.firstName = item.name.first;
+                contact.lastName = item.name.last;
+                contact.email = item.email;
+                contact.city = item.location.city;
+
+                // console.log('--------')
+                // console.log(contact)
+
+                this.listContacts.push(contact);
+            });
         }
     }
 })
-
 app.mount('#app')
